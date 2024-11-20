219b36fdd6d004493213a07d202d9230e6a73047
amonroca <roc23009@byui.edu>
2024-11-20 10:54:45 -0300

    [week 02] changing tag picture to figure and removing commented code from temples.js file

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,38 +1,3 @@
-/*async function fetchTemples() {
-    const response = await fetch('https://byui-cse.github.io/cse121b-ww-course/resources/temples.json');
-    const temples = await response.json();
-    return temples;
-}
-
-function createTempleCard(temple) {
-    const card = document.createElement('div');
-    card.className = 'temple-card';
-
-    const name = document.createElement('h2');
-    name.textContent = temple.templeName;
-    card.appendChild(name);
-
-    const image = document.createElement('img');
-    image.src = temple.imageUrl;
-    image.alt = temple.templeName;
-    card.appendChild(image);
-
-    return card;
-}
-
-async function displayTemples() {
-    const temples = await fetchTemples();
-    const templeContainer = document.getElementById('temple-container');
-
-    temples.forEach(temple => {
-        const templeCard = createTempleCard(temple);
-        templeContainer.appendChild(templeCard);
-    });
-}
-
-// Call the displayTemples function when the page loads
-window.onload = displayTemples;*/
-
 const hamButton = document.querySelector('#menu');
 const navigation = document.querySelector('.navigation');
 
